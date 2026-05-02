53794eec5c17ad9b6a87399db326c44344070c88
ljusvera <promaykil1@gmail.com>
2026-05-02 16:30:35 +0200

    Update ai_app.py

--- ai_app.py
+++ ai_app.py
@@ -1,9 +1,19 @@
 import streamlit as st
+from openai import OpenAI
 
-st.title("ljus")
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-savol = st.text_input("kimsan:")
+st.title("Mening AI yordamchim 🤖")
+
+savol = st.text_input("Savol yoz:")
 
 if savol:
-    javob = f"Savoling: {savol}. Men hali o‘rganayapman 😄"
-    st.write(javob)
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": savol}
+        ]
+    )
+
+    javob = response.choices[0].message.content
+    st.write("🤖:", javob)
